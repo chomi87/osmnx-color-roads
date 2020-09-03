a1c6a4f81b534717bd81b70dc0994867c05d80ea
Michael Chong <mc@Michaels-MacBook-Pro.local>
2020-09-03 14:38:59 +0200

    added color selector and wait for it message

--- src/app.py
+++ src/app.py
@@ -2,10 +2,18 @@ import streamlit as st
 from osmnx_color_roads import generate_image
 import pandas as pd
 st.title('Color city generator')
+
+# Add a widget to the sidebar to select number of colors:
+key_size = st.sidebar.number_input(
+    'How many colors?',
+    1, 20, 10
+)
+
 city = ""
 city = st.text_input("city", value='', max_chars=None, key=None, type='default')
 st.write('You selected: ', city)
 
 if len(city)>2:
-    fig, a,b,c  = generate_image(city, query_type='string', key_size=9, line_width=0.5)
+    with st.spinner('Wait for it...'):
+        fig, top, popular_words, palette_key  = generate_image(city, query_type='string', key_size=key_size, line_width=0.5)
     st.pyplot()
