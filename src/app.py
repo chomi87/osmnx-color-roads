92a2ee60984deeff522f92c68530497fd621c6f0
Michael Chong <mc@Michaels-MacBook-Pro.local>
2020-09-03 12:03:24 +0200

    handle no input case

--- src/app.py
+++ src/app.py
@@ -2,9 +2,10 @@ import streamlit as st
 from osmnx_color_roads import generate_image
 import pandas as pd
 st.title('Color city generator')
+city = ""
 city = st.text_input("city", value='', max_chars=None, key=None, type='default')
-
 st.write('You selected: ', city)
 
-fig, a,b,c  = generate_image(city, query_type='string', key_size=9, line_width=0.5)
-st.pyplot()
+if len(city)>2:
+    fig, a,b,c  = generate_image(city, query_type='string', key_size=9, line_width=0.5)
+    st.pyplot()
